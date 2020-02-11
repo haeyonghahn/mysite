176f56f4ac39bf51dfea742ff6799668512d1129
haeyonghahn <youg1322@naver.com>
2020-02-11 10:33:20 +0900

    init commit

---
+++ mysite02/src/main/java/com/douzone/web/listener/ContextLoadListener.java
@@ -0,0 +1,23 @@
+package com.douzone.web.listener;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+@WebListener
+public class ContextLoadListener implements ServletContextListener {
+
+    // 어플리케이션이 올라갔을 때 (아까는 서블릿에서 초기화하는 것임. 이거는 톰캣에서 초기화. 범위가 더 넓다)
+    public void contextInitialized(ServletContextEvent servletContextEvent)  { 
+    	ServletContext context = servletContextEvent.getServletContext();
+    	String contextConfigLocation = context.getInitParameter("contextConfigLocation");
+    	System.out.println("Application Starts..." + contextConfigLocation);
+    }
+    
+    // 톰캣 내려갈 때
+    public void contextDestroyed(ServletContextEvent arg0)  { 
+        
+    }
+	
+}
