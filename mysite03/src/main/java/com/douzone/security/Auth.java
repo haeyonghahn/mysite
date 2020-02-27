c7ebfd6b04daed314f00d0d91043eee45f91e3ef
User <User@DESKTOP-FQTA9PE>
2020-02-27 16:44:31 +0900

    어드민 접근 제한 구현

--- mysite03/src/main/java/com/douzone/security/Auth.java
+++ mysite03/src/main/java/com/douzone/security/Auth.java
@@ -8,8 +8,7 @@ import java.lang.annotation.Target;
 @Target({ElementType.METHOD, ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Auth {
-	
-public enum Role {USER, ADMIN}
+	public enum Role {USER, ADMIN}
 	
 	String value() default "USER";
 
