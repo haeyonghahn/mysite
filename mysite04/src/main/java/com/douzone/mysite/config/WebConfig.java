b96423758ca55021173a599bfff65cbb6aaf9026
User <User@DESKTOP-FQTA9PE>
2020-03-25 12:13:31 +0900

    WebConfig #1

---
+++ mysite04/src/main/java/com/douzone/mysite/config/WebConfig.java
@@ -0,0 +1,10 @@
+package com.douzone.mysite.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+//@Import({MVCConfig.class, SecurityConfig.class, MessageConfig.class, FileUploadConfig.class})
+public class WebConfig {
+
+}
