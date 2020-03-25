927e6424820efd9e4c757fb8be6a4c7153aaad12
haeyonghahn <youg1322@naver.com>
2020-03-25 15:39:46 +0900

    prperty 설정 & message source, MessageConfig

--- mysite04/src/main/java/com/douzone/mysite/config/WebConfig.java
+++ mysite04/src/main/java/com/douzone/mysite/config/WebConfig.java
@@ -5,13 +5,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Import;
 
+import com.douzone.mysite.config.web.FileUploadConfig;
+import com.douzone.mysite.config.web.MessageConfig;
 import com.douzone.mysite.config.web.MvcConfig;
 import com.douzone.mysite.config.web.SecurityConfig;
 
 @Configuration
 @EnableAspectJAutoProxy
 @ComponentScan({"com.douzone.mysite.controller", "com.douzone.mysite.exception"})
-@Import({MvcConfig.class, SecurityConfig.class})
+@Import({MvcConfig.class, SecurityConfig.class, MessageConfig.class, FileUploadConfig.class})
 public class WebConfig {
 
 }
