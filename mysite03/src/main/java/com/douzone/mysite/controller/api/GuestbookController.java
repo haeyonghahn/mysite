ace88ba59cb8d9418a593db56275b6dd3b5c4c5a
haeyong Hahn <youg1322@naver.com>
2020-04-16 12:11:59 +0900

    ajax guestbook #3

--- mysite03/src/main/java/com/douzone/mysite/controller/api/GuestbookController.java
+++ mysite03/src/main/java/com/douzone/mysite/controller/api/GuestbookController.java
@@ -3,14 +3,16 @@ package com.douzone.mysite.controller.api;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.douzone.mysite.vo.GuestBookVo;
-
 import com.douzone.mysite.dto.JsonResult;
 import com.douzone.mysite.service.GuestBookService;
+import com.douzone.mysite.vo.GuestBookVo;
 
 @RestController("GuestbookApiController")	// return하는 값들이 객체로 생성되서 @Responsebody를 안붙여도 된다.
 @RequestMapping("/api/guestbook")
@@ -19,9 +21,16 @@ public class GuestbookController {
 	@Autowired
 	private GuestBookService guestbookService;
 
-	@RequestMapping("/list/{no}")
+	@GetMapping("/list/{no}")
 	public JsonResult list(@PathVariable("no") Long startNo) {
 		List<GuestBookVo> list = guestbookService.getMessageList(startNo);
 		return JsonResult.success(list);
+	}
+	
+	@PostMapping("/add")
+	public JsonResult add(@RequestBody GuestBookVo vo) {
+		guestbookService.add(vo);
+		vo.setPassword("");
+		return JsonResult.success(vo);
 	}
 }
